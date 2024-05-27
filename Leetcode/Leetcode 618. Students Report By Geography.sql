fa2e6c69d828559a3f99635fc3f5858f82c599db
Ashank <42887373+AshankMore@users.noreply.github.com>
2024-05-27 08:39:50 -0400

    Add files via upload

---
+++ Leetcode/Leetcode 618. Students Report By Geography.sql
@@ -0,0 +1,14 @@
+select
+max(case when continent='America' then name end) as America
+,max(case when continent='Asia' then name end) as Asia
+,max(case when continent='Europe' then name end) as Europe
+from
+(
+select
+continent,
+name,
+row_number() over (partition by continent order by name) as rn
+from
+Student
+)i
+group by rn
