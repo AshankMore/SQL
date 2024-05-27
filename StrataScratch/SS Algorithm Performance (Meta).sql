901f564f7eb70ae1a9ee4a0987af80828236f103
Ashank <42887373+AshankMore@users.noreply.github.com>
2024-05-27 08:46:49 -0400

    Add files via upload

---
+++ StrataScratch/SS Algorithm Performance (Meta).sql
@@ -0,0 +1,13 @@
+SELECT SEARCH_ID, MAX(rating)
+from
+(
+SELECT
+search_id, 
+case when clicked = 0 then 1 
+     when clicked = 1 and search_results_position > 3 then 2
+     when clicked = 1 and search_results_position <= 3 then 3
+     else Null 
+     end as rating
+from fb_search_events
+) p
+group by 1
