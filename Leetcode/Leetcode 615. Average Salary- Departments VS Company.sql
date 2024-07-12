4f3ff278df37a4a68be4738864022a5f15cd870f
Ashank <42887373+AshankMore@users.noreply.github.com>
2024-07-12 07:58:29 -0400

    Update Leetcode 615. Average Salary- Departments VS Company.sql

--- Leetcode/Leetcode 615. Average Salary- Departments VS Company.sql
+++ Leetcode/Leetcode 615. Average Salary- Departments VS Company.sql
@@ -12,23 +12,4 @@ SELECT T2.pay_month, T2.department_id,
 (case when dept_avg > comp_avg then 'higher' 
      when dept_avg < comp_avg then 'lower'
      else 'same' end ) as comparison FROM T2 LEFT JOIN T1 ON T2.pay_month = T1.pay_month 
-order by 1 
-
-/*
-SELECT DISTINCT pay_month, department_id, 
-(
-    CASE
-    WHEN avg_dept_sal > avg_company_sal THEN "higher"
-    WHEN avg_dept_sal < avg_company_sal THEN "lower"
-    WHEN avg_dept_sal = avg_company_sal THEN "same"
-    END
-) as comparison
-FROM 
-(
-    SELECT s.employee_id, amount, pay_Date, department_id, LEFT(s.pay_date, 7) as pay_month,
-     AVG(s.amount) OVER(PARTITION BY s.pay_date) as avg_company_sal,
-     AVG(s.amount) OVER(PARTITION BY s.pay_Date, e.department_id) as avg_dept_sal
-    FROM Salary s
-    JOIN Employee e ON s.employee_id = e.employee_id
-) as tmp;
-*/
+order by 1 ;
