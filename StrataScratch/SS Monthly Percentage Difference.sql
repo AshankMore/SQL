258cfb324fa1d28f276fc7a8a781d97113374ddc
Ashank <42887373+AshankMore@users.noreply.github.com>
2024-07-13 20:49:58 -0400

    Update SS Monthly Percentage Difference.sql

--- StrataScratch/SS Monthly Percentage Difference.sql
+++ StrataScratch/SS Monthly Percentage Difference.sql
@@ -1,10 +1,11 @@
-WITH t1 as (
+WITH t1 as 
+        (
         SELECT  to_char(created_at, 'YYYY-MM') as date_month,
         SUM(value) as revenue,
         LAG(sum(value), 1) over(order by to_char(created_at, 'YYYY-MM')) as prev_revenue 
         FROM sf_transactions
         GROUP BY to_char(created_at, 'YYYY-MM')
-)
+        )
 
 SELECT  date_month,
         round((revenue - prev_revenue) / prev_revenue * 100, 2) as revenue_diff_pct
